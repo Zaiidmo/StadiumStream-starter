a1f8b89e3141830d831a6a8dc108371b4d34ba5d
Zaiid Moumnii <zaiidmoumnii@gmail.com>
2023-12-20 18:52:11 +0100

    Creating dashboard navigation

---
+++ dashboard.php
@@ -0,0 +1,22 @@
+<?php 
+require './app/Controller/Config.php'; 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="./Public/dist/output.css" />
+    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+
+</head>
+<body class="dark:bg-gray-900 bg-gray-300">
+    <?php include './includes/dashboard_navigation.php' ?>
+    
+
+  
+    
+</body>
+<script src="./Public/js/theme.js"></script>
+</html>
---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+// module.exports = {
+//     plugins: [
+//       require('tailwindcss'),
+//       require('autoprefixer'),
+//     ],
+//   }
+  
+module.exports = {
+  plugins: {
+    'postcss-preset-env': {},
+    autoprefixer: {},
+    // Add other PostCSS plugins here if needed
+  }
+}
